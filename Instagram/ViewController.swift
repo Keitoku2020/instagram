e230cdf9abe34d90958e806d3e9f025cc225e6b9
keitoku.ikegami <keitoku.ikegami2020@klas.ch>
2020-04-25 14:48:12 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by IKEGAMIKeitoku on 25.04.20.
+//  Copyright © 2020 keitoku.ikegami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
